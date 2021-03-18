ae2b126cf8cf38b0d6567aac1739767e2fb91fdb
MarcinGruchala <margruchala@gmail.com>
2021-03-18 22:10:56 +0000

    Resolve "Design user interface"

--- app/src/main/java/com/example/chessboard_importer/MainActivity.kt
+++ app/src/main/java/com/example/chessboard_importer/MainActivity.kt
@@ -1,11 +1,28 @@
 package com.example.chessboard_importer
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.chessboard_importer.databinding.ActivityMainBinding
 
+private lateinit var binding: ActivityMainBinding
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        binding.btnTakePhoto.setOnClickListener {
+            Intent(this, PhotoPreviewActivity::class.java ).also {
+                startActivity(it)
+            }
+        }
+
+        binding.btnImportPhoto.setOnClickListener {
+            Intent(this, PhotoPreviewActivity::class.java ).also {
+                startActivity(it)
+            }
+        }
     }
 }
