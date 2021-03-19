4fbdcd6bebdf6eb133b58b07b10f1dea63b907aa
MarcinGruchala <margruchala@gmail.com>
2021-03-19 16:11:00 +0000

    Resolve "Implement taking photos (REQ-1)"

--- app/src/main/java/com/example/chessboard_importer/MainActivity.kt
+++ app/src/main/java/com/example/chessboard_importer/MainActivity.kt
@@ -1,9 +1,20 @@
 package com.example.chessboard_importer
 
+import android.app.Activity
 import android.content.Intent
+import android.graphics.BitmapFactory
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Environment
+import android.provider.MediaStore
+import androidx.core.content.FileProvider
 import com.example.chessboard_importer.databinding.ActivityMainBinding
+import java.io.File
+
+private const val TAKE_PHOTO_REQUEST_CODE=42
+private const val FILE_NAME = "photo.jpg"
+lateinit var photoFile: File
 
 private lateinit var binding: ActivityMainBinding
 class MainActivity : AppCompatActivity() {
@@ -14,9 +25,11 @@ class MainActivity : AppCompatActivity() {
         setContentView(view)
 
         binding.btnTakePhoto.setOnClickListener {
-            Intent(this, PhotoPreviewActivity::class.java ).also {
-                startActivity(it)
-            }
+            val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            photoFile = getPhotoFile(FILE_NAME)
+            val fileProvider  = FileProvider.getUriForFile(this,"com.example.provider.fileprovider", photoFile)
+            takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT,fileProvider)
+            startActivityForResult(takePictureIntent, TAKE_PHOTO_REQUEST_CODE)
         }
 
         binding.btnImportPhoto.setOnClickListener {
@@ -24,5 +37,19 @@ class MainActivity : AppCompatActivity() {
                 startActivity(it)
             }
         }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == TAKE_PHOTO_REQUEST_CODE && resultCode== Activity.RESULT_OK){
+            Intent(this, PhotoPreviewActivity::class.java ).also {
+                startActivity(it)
+            }
+        }
+    }
+
+    private fun getPhotoFile(fileName: String): File {
+        val storageDirectory = getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+        return File.createTempFile(fileName,".jpg",storageDirectory)
     }
 }
